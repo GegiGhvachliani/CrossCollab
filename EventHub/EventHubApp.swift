1a3864254e4f68cf922777119e79b7459248fc7f
Gegi Ghvachliani <GegiGhvachliani@gmail.com>
2025-12-21 00:02:20 +0400

    add project

---
+++ EventHub/EventHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubApp.swift
+//  EventHub
+//
+//  Created by Gegi Ghvachliani on 20.12.25.
+//
+
+import SwiftUI
+
+@main
+struct EventHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
